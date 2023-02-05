643671d0c13748b383eadfae7e048ab30d03070b
Vignesh <emvignesh16@gmail.com>
2023-02-05 15:25:04 +0530

    Update Home page

--- app/src/main/java/com/example/listbenchmark/MainActivity.kt
+++ app/src/main/java/com/example/listbenchmark/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.listbenchmark
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.listbenchmark.ui.home.HomeListItem
+import com.example.listbenchmark.ui.home.HomeScreen
 import com.example.listbenchmark.ui.theme.ListBenchmarkTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +24,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val homeListItem = listOf(
+                        HomeListItem.ComposeLazyColumn,
+                        HomeListItem.NativeRecyclerView,
+                        HomeListItem.ComposeViewInRecyclerView,
+                        HomeListItem.XmlViewComposeViewInRecyclerView
+                    )
+                    HomeScreen(homeListItem, ::onClick)
                 }
             }
         }
+    }
+
+    private fun onClick(item: HomeListItem) {
+        Log.d(TAG, "onClick: ${item.title}")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
---
+++ app/src/main/java/com/example/listbenchmark/ui/home/HomeScreen.kt
@@ -0,0 +1,49 @@
+package com.example.listbenchmark.ui.home
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(homeList: List<HomeListItem>, onClick: (HomeListItem) -> Unit) {
+    LazyColumn {
+        items(homeList) {
+            HomeListItem(it, onClick)
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun HomeListItem(item: HomeListItem, onClick: (HomeListItem) -> Unit) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .heightIn(56.dp)
+            .fillMaxWidth()
+            .clickable { onClick.invoke(item) }
+    ) {
+        Text(
+            text = item.title,
+            modifier = Modifier
+                .padding(horizontal = 8.dp, vertical = 16.dp)
+        )
+    }
+}
+
+sealed class HomeListItem(internal val title: String) {
+    object ComposeLazyColumn : HomeListItem("Compose LazyColumn")
+    object NativeRecyclerView : HomeListItem("Native RecyclerView")
+    object ComposeViewInRecyclerView : HomeListItem("ComposeView + RecyclerView")
+    object XmlViewComposeViewInRecyclerView : HomeListItem("XML View + ComposeView + RecyclerView")
+}
